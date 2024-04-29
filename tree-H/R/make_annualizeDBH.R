7f60d4df8074502fa78065fa1d5574d1a3a6fe45
Ceci Martinez <cmartinez3@arizona.edu>
2024-04-29 15:22:53 -0700

    reworking the backcalculation of dbh function

--- tree-H/R/make_annualizeDBH.R
+++ tree-H/R/make_annualizeDBH.R
@@ -7,23 +7,28 @@
 ## modeified by Ceci Martinez
 ## backcalculate DBH usng ringwidth data
 
-backcalculate_DBH <- function(TRE_CN,DIA_t,MEASYEAR,Year,RW){
+backcalculate_DBH <- function(dat_rw, dat_dbh){
+  #TRE_CN,DIA_t,MEASYEAR,Year,RW - need these columns in dataframes 
   #create data frame with empty column for annualized dbh
-  tree_df <- data.frame(TRE_CN,DIA_t,MEASYEAR,Year,RW,DIA_bc = NA)
+  #tree_df <- data.frame(TRE_CN,DIA_t,MEASYEAR,Year,RW,DIA_bc = NA)
   #N is the row where measure year and ring width year are the same
-  N <- which(tree_df$Year == tree_df$MEASYEAR[1]) #next step is to allow N to be ring width year -1
+  N <- nrow(dat_rw)
+  dia_bc <- rep(0, N)
+  
+  
+  N <- which(dat_rw$Year == dat_dbh$MEASYEAR[1]) #next step is to allow N to be ring width year -1
   if(length(N) == 0){
-    N <- which(tree_df$Year + 1 == tree_df$MEASYEAR[1])
+    N <- which(dat_rw$Year + 1 == dat_dbh$MEASYEAR[1])
   }
   if(length(N) > 0){
     Curr_row <- N-1 #each time through subtract 1 and move down one row (or back one year)
-    tree_df$DIA_bc[N] <- tree_df$DIA_t[N] #dbh when year of ring width and measure year are equal
-    while (Curr_row > 0 & !is.na(tree_df$DIA_bc[Curr_row + 1])) { #loop will stop when it gets to the end of data for that tree
-      DIA_1 <- tree_df$DIA_bc[Curr_row+1] #or DIA_t[N] for the first round
-      RW1 <- tree_df$RW[Curr_row+1] 
+    dia_bc[N] <- dat_dbh$DIA_t[N] #dbh when year of ring width and measure year are equal
+    while (Curr_row > 0 & !is.na(dia_bc[Curr_row + 1])) { #loop will stop when it gets to the end of data for that tree
+      DIA_1 <- dia_bc[Curr_row+1] #or DIA_t[N] for the first round
+      RW1 <- dat_rw$RW[Curr_row+1] 
       #convert ring width from mm to inches
       RW1 = RW1 * 0.0393701
-      tree_df$DIA_bc[Curr_row] <- DIA_1 - (2*RW1) ## okay this is just the absolute ring width way right now
+      dia_bc[Curr_row] <- DIA_1 - (2*RW1) ## okay this is just the absolute ring width way right now
       #continue loop for next row until curr_row>0
       Curr_row = Curr_row - 1 
     }
