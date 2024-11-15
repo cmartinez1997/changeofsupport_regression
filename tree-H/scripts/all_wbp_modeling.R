1135c85943cc595c1e6c924301412250d57ade66
Ceci Martinez <122909681+cmartinez1997@users.noreply.github.com>
2024-11-15 12:42:23 -0700

    commit from meeting 11/15

--- tree-H/scripts/all_wbp_modeling.R
+++ tree-H/scripts/all_wbp_modeling.R
@@ -15,6 +15,7 @@ library(ggplot2)
 library(dplyr)
 library(lme4)
 library(glmnet)
+library(stringr)
 
 ######################### Analysis for Whitebark Pine Climate-Growth##########################################
 
@@ -29,24 +30,20 @@ source(here::here("tree-H", "R", "make_seasonalwindows.R"))
 dat <- read_csv(here::here("tree-H", "data", "processed", "wbp_all_climate_growth_rw.csv"))
 dat_bc <- read_csv(here::here("tree-H", "data", "processed", "wbp_size_all.csv"))
 #if you want climate scaled, then load "wbp_all_climate_data_scaled_all.csv"
-dat_climate <- read_csv(here::here("tree-H", "data", "processed", "wbp_all_climate_data_all.csv"))# Check the data sources for overlap and lack of overlap
+dat_climate <- read_csv(here::here("tree-H", "data", "processed", "clim_local_scale.csv"))# Check the data sources for overlap and lack of overlap
 
-missing_overlap <- check_overlap(dat, dat_climate, dat_bc)
-
-# Drop the missing data # do this for backcaluation
-message("About ", round(mean((missing_overlap$tree_CN_missing) | (missing_overlap$tree_year_missing)) * 100, digits = 0), "% of tree ring data will be dropped")
-message("About ", round(mean((missing_overlap$climate_CN_missing) | (missing_overlap$climate_year_missing)) * 100, digits = 0), "% of climate ring data will be dropped")
-message("About ", round(mean((missing_overlap$bc_tree_CN_missing) | (missing_overlap$bc_year_missing)) * 100, digits = 0), "% of backcalculated data will be dropped")
-
-dat         <- dat[!missing_overlap$tree_CN_missing & !missing_overlap$tree_year_missing, ]
-dat_climate <- dat_climate[!missing_overlap$climate_CN_missing & !missing_overlap$climate_year_missing, ]
-dat_climate <- dat_climate %>% 
-  dplyr::select(-tmin)
-dat_bc      <- dat_bc[!missing_overlap$bc_tree_CN_missing & !missing_overlap$bc_year_missing, ]
-
-
-
-
+# missing_overlap <- check_overlap
+# 
+# # Drop the missing data # do this for backcaluation
+# message("About ", round(mean((missing_overlap$tree_CN_missing) | (missing_overlap$tree_year_missing)) * 100, digits = 0), "% of tree ring data will be dropped")
+# message("About ", round(mean((missing_overlap$climate_CN_missing) | (missing_overlap$climate_year_missing)) * 100, digits = 0), "% of climate ring data will be dropped")
+# message("About ", round(mean((missing_overlap$bc_tree_CN_missing) | (missing_overlap$bc_year_missing)) * 100, digits = 0), "% of backcalculated data will be dropped")
+# 
+# dat         <- dat[!missing_overlap$tree_CN_missing & !missing_overlap$tree_year_missing, ]
+# dat_climate <- dat_climate[!missing_overlap$climate_CN_missing & !missing_overlap$climate_year_missing, ]
+# dat_climate <- dat_climate %>% 
+#   dplyr::select(-tmin)
+# dat_bc      <- dat_bc[!missing_overlap$bc_tree_CN_missing & !missing_overlap$bc_year_missing, ]
 
 
 # Create the size backcalculated matrix
@@ -63,61 +60,85 @@ data.frame(Z = Z, Year = year_id_Z, TRE_CN = tree_id) %>%
   left_join(dat) 
 
 
-dat_fit <- dat %>% left_join(dat_bc) %>% left_join(data.frame(Z = Z, Year = year_id_Z, TRE_CN = tree_id))
+dat_fit <- dat %>% left_join(dat_bc) %>% left_join(data.frame(Z = Z, Year = year_id_Z, TRE_CN = tree_id)) %>% 
+  rename(year = Year)
+dat_fit$PLOT_CN <- as.character(dat_fit$PLOT_CN)
+dat_climate$PLOT_CN <- as.character(dat_climate$PLOT_CN)
+
+
 # so we have to create a dataframe to account for seasonal, or time varying monthly climate variables
-dat_all <- dat_fit  %>%  right_join(dat_climate, by = join_by(PLOT_CN == PLOT_CN, Year == growthyear)) %>% 
-  dplyr::select(-year) %>% 
-  filter(!(TRE_CN %in% c("23R48", "23T255"))) # this gets rid of weird outliers
+# dat_all <- dat_fit  %>%  right_join(dat_climate, by = join_by(PLOT_CN == PLOT_CN, Year == year)) %>% 
+#   dplyr::select(-year) %>% 
+  # filter(!(TRE_CN %in% c("23R48", "23T255"))) # this gets rid of weird outliers
 
-dat_all_seas <- dat_fit %>% right_join(seasonal_clim_dat, by = join_by(PLOT_CN == PLOT_CN, Year == year))
+dat_all_seas <- dat_fit %>% left_join(dat_climate, by = c("PLOT_CN", "year"))
   
+#okay now remove all rows where climate data = NA.  ###THIS IS THE FINAL DATAFRAME W WILL USE FOR ANALYSIS
+dat_all_seas <- dat_all_seas %>%
+  filter(!is.na(pJunAug_tmax)) %>% 
+  filter(!(TRE_CN %in% c("23R48", "23T255")))
 
-# dat_fit for bc and climate data
-# align climate to the dataframe - maybe through another join (choose vars well) join_by !!! 
-# marginal effect of size
-dat_all %>% 
-  ggplot(aes(y = log(RW + 0.001), x = Z, color = TRE_CN) ) + 
-  # geom_point() + 
-  geom_line() + 
-  stat_smooth(aes(y = log(RW + 0.001), x = Z), inherit.aes = FALSE, 
-              method = "lm", formula = y ~ x + I(x^2))+ 
-  stat_smooth(aes(y = log(RW + 0.001), x = Z), inherit.aes = FALSE, 
-              color = "black", method = "gam") +
-  theme_bw() + 
-  theme(legend.position = "none")  # Remove the legend
 
-#run model with aligned data frame
+################################# MODEL BUILDING #######################################
+############## MODEL 1 ########################
 
-mod_size <- lm(log(RW + 0.001) ~ Z + I(Z^2), data = dat_all)
+# this model is only a size model 
+mod_size <- lm(log(RW + 0.001) ~ Z + I(Z^2), data = dat_all_seas)
 summary(mod_size) #matrices align issue - redo the matrix 
 plot(mod_size)
 check_model(mod_size)
 
-mod_climate_01 <- lm(log(RW + 0.001) ~ Z + I(Z^2) + precip, data = dat_all)
+############## MODEL 2 ########################
+
+# model with MAP
+mod_climate_01 <- lm(log(RW + 0.001) ~ Z + I(Z^2) + precip, data = dat_all_seas)
 summary(mod_climate_01) 
+check_model(mod_climate_01)
 
-mod_climate_02 <- lm(log(RW + 0.001) ~ Z + I(Z^2) + precip + meantemp, data = dat_all)
+############## MODEL 3 ########################
+
+# model with MAP/MAT
+mod_climate_02 <- lm(log(RW + 0.001) ~ Z + I(Z^2) + precip + meantemp, data = dat_all_seas)
 summary(mod_climate_02) 
+check_model(mod_climate_02)
 
-#wrangling to compare time varying climate
+############## MODEL 4 ########################
 
-dat_wide <- dat_all %>% pivot_wider(names_from = month, values_from = tmax:ppt) %>% 
-  dplyr::select(-c("PLOT_CN", "Year", "MEASYEAR", "DIA")) 
-
-idx <- dat_wide$RW == 0
-dat_wide$RW[idx] <- NA # getting rid of outlier residual
-
-#mod with time varying climate predictors
-
-dat_wide_state <- dat_wide %>%
-  left_join(wbp_meta_all %>% dplyr::select(TRE_CN, STATECD), by = "TRE_CN")
-
-mod_climate_03 <- lm(log(RW + 0.001) ~ Z + I(Z^2) + . -TRE_CN, data = dat_wide)
+# model with MAP/MAT and aggregated time varying precip 
+mod_climate_03 <- lm(log(RW + 0.001) ~ Z + I(Z^2) +  
+                     prevJun_currAug_ppt + precip + meantemp, 
+                     data = dat_all_seas)
 summary(mod_climate_03) 
 check_model(mod_climate_03)
-plot(mod_climate_03)
 
-model_summary <- broom::tidy(mod_climate_03, conf.int = TRUE)
+############## MODEL 5 ########################
+
+# model with MAP/MAT aggregated time varying precip and previous summer tmax
+mod_climate_04 <- lm(log(RW + 0.001) ~ Z + I(Z^2) + prevJun_currAug_ppt + pJunAug_tmax + precip + meantemp, 
+     data = dat_all_seas)
+summary(mod_climate_04) 
+check_model(mod_climate_04) ###Best model with AIC
+
+
+############## MODEL 6 ######################## Alll of the vars
+mod_climate_05 <- lm(log(RW + 0.001) ~ Z + I(Z^2) + JunAug_ppt + pJunAug_ppt + pJunAug_JunAug_ppt + 
+                       JunAug_tmax + prevJun_currAug_ppt + pJunAug_tmax + AprMay_tmax + pAprMay_tmax + precip + meantemp, 
+                     data = dat_all_seas)
+summary(mod_climate_05) 
+check_model(mod_climate_05)
+
+#another model 
+mod_climate_06 <- lm(log(RW + 0.001) ~ Z + I(Z^2) + prevJun_currAug_ppt + pJunAug_tmax + pAprMay_tmax + precip + meantemp, 
+                     data = dat_all_seas)
+summary(mod_climate_06) 
+check_model(mod_climate_06) ###Best model with AIC
+
+
+############## Checking models with AIC ######################## 
+AIC(mod_climate_01, mod_climate_02, mod_climate_03, mod_climate_04, mod_climate_06)
+
+#best model with AIC is Model 4
+model_summary <- broom::tidy(mod_climate_04, conf.int = TRUE)
 model_summary <- model_summary %>% filter(term != "(Intercept)")
 
 ggplot(model_summary, aes(x = term, y = estimate, ymin = conf.low, ymax = conf.high)) +
@@ -129,37 +150,120 @@ ggplot(model_summary, aes(x = term, y = estimate, ymin = conf.low, ymax = conf.h
   theme_minimal()
 
 
+############## MODEL 6 ######################## Alll of the vars
 # Add random effect structure 
+variable_ppt <- c("JunAug_ppt", "pJunAug_ppt", "pJunAug_JunAug_ppt", "prevJun_currAug_ppt")
+variable_tmax <- c("JunAug_tmax", "pJunAug_tmax", "AprMay_tmax", "pAprMay_tmax")
+f4 <- as.formula(paste("log(RW + 0.001) ~ Z + I(Z^2) + meantemp + precip", paste(c(variable_ppt, variable_tmax), collapse = " + "), sep = " + "))
 
-variable_ppt <- c("ppt_6", "ppt_7", "ppt_8")
-variable_tmax <-  c("tmax_5", "tmax_6", "tmax_7", "tmax_8")
-f4 <- paste("log(RW + 0.001) ~ Z + I(Z^2) + meantemp + precip", str_flatten(variable_ppt, collapse = " + "), sep = " + ")
 
-stgrmod_climate_04 <- lm(f4, data = dat_wide)
-mod_climate_05 <- lmer(paste(f4, "(1|TRE_CN)", sep = " + "), data = dat_wide)
+mod_climate_RE <- lmer(log(RW + 0.001) ~  Z + I(Z^2) + prevJun_currAug_ppt + 
+                         JunAug_tmax + meantemp + precip + (1 + Z | TRE_CN), data = dat_all_seas)
 
-## slope effect
-mod_climate_05 <- lmer(paste(f4, "(1|TRE_CN) + (1+Z|", sep = " + "), data = dat_wide)
+mod_climate_RE_z <- lmer(log(RW + 0.001) ~  Z + (prevJun_currAug_ppt + 
+                         JunAug_tmax + meantemp + precip)^2 + (1 + Z | TRE_CN), data = dat_all_seas)
 
-AIC(mod_climate_04,mod_climate_05)
-check_model(mod_climate_04)
+mod_climate_RE_z <- lmer(log(RW + 0.001) ~  Z + prevJun_currAug_ppt + 
+                                                   JunAug_tmax + meantemp*JunAug_tmax + precip*prevJun_currAug_ppt + (1 + Z | TRE_CN), data = dat_all_seas)
 
-x <- model.matrix(log(RW + 0.001) ~ Z + I(Z^2) + . - 1 - TRE_CN, data = dat_wide)
-y <- dat_wide %>% 
+anova(mod_climate_RE, mod_climate_RE_z)
+
+preds <- predict(mod_climate_RE_z)
+str(preds)
+
+all_cns <- unique(dat_all_seas$TRE_CN)
+n_plot  <- 36
+
+plot_cns <- sample(all_cns, n_plot)
+
+dat_all_seas %>% drop_na() %>% 
+  mutate(preds = preds) %>% 
+  filter(TRE_CN %in% plot_cns) %>% 
+  ggplot() + geom_point(aes(x = Z, y = (RW), colour = TRE_CN)) + theme(legend.position = "none")  +
+ stat_smooth(aes(x = Z, y = (RW), colour = TRE_CN), method = "lm") + 
+  theme_classic() + facet_wrap(~TRE_CN)
+
+dat_all_seas %>% drop_na() %>% 
+  mutate(preds = preds) %>% 
+  filter(TRE_CN %in% plot_cns) %>% 
+  ggplot()  +
+  geom_point(aes(x = Z, y = (RW), colour = TRE_CN)) + 
+  stat_smooth(aes(x = Z, y = (RW), colour = TRE_CN), method = "lm", se = F) + 
+  stat_smooth(aes(x = Z, y = (RW)), color = "blue", method = "lm", se = F, linewidth = 2) + 
+  theme_classic() + theme(legend.position = "none") + facet_wrap(~TRE_CN)
+
+dat_all_seas %>% drop_na() %>% 
+  mutate(preds = preds) %>%
+ filter(TRE_CN %in% plot_cns) %>% 
+  ggplot()  +
+  geom_point(aes(x = Z, y = (RW), colour = TRE_CN)) + 
+  stat_smooth(aes(x = Z, y = (RW), colour = TRE_CN), method = "lm", se = F, formula = y~ x + I(x^2)) + 
+  stat_smooth(aes(x = Z, y = (RW)), color = "blue", method = "lm", se = F, linewidth = 2, formula = y~ x + I(x^2)) + 
+  theme_classic() + theme(legend.position = "none") + facet_wrap(~TRE_CN)
+
+model_summary <- broom::tidy(mod_climate_RE_z, conf.int = TRUE)
+model_summary <- model_summary %>% filter(term != "(Intercept)")
+
+ggplot(model_summary, aes(x = term, y = estimate, ymin = conf.low, ymax = conf.high)) +
+  geom_pointrange(color = "cadetblue") +
+  coord_flip() +  # Flip coordinates for horizontal plot
+  geom_hline(yintercept = 0, color = "black", linetype = "dashed",size = 1) +  labs(
+    x = "Coefficients", 
+    y = "Estimate (with 95% CI)") +
+  theme_minimal()
+
+### OKAY comparing Mixed effects and looking at some model diagnostics
+
+par(mfrow = c(1, 2))
+plot(fitted(mod_climate_RE), resid(mod_climate_RE), main = "Mixed Effects")
+plot(fitted(mod_climate_04), resid(mod_climate_04), main = "Fixed Effects")
+
+par(mfrow = c(1, 2))
+
+# QQ plot for mixed-effects model residuals
+qqnorm(resid(mod_climate_RE), main = "QQ Plot: Mixed Effects")
+qqline(resid(mod_climate_RE), col = "red")
+
+# QQ plot for fixed-effects model residuals
+qqnorm(resid(mod_climate_04), main = "QQ Plot: Fixed Effects")
+qqline(resid(mod_climate_04), col = "blue")
+
+
+#R^2 for mixed effects models, this uses the performance package
+r2_mixed <- r2(mod_climate_RE)
+print(r2_mixed)
+
+#R^2 for fixed effects models
+r2_fixed <- summary(mod_climate_04)$r.squared
+print(r2_fixed)
+
+
+
+###Regularization###
+# using lassos
+
+
+dat_all_seas <- dat_all_seas %>% 
+  select(-DIA)
+x <- model.matrix(log(RW + 0.001) ~ Z + I(Z^2) + . - 1 - TRE_CN -PLOT_CN -year, data = dat_all_seas)
+y <- dat_all_seas %>% 
   drop_na() %>% 
   pull(RW)
 str(y)
-# mod_net <- cv.glmnet(x,log(y + 0.001))
-# summary(mod_net)
-# plot(mod_net)
-# coef(mod_net, s = "lambda.min")
 
-mod_ad <- glmnet(x, y)
-print(mod_ad)
-mod_net <- cv.glmnet(x, y)
-print(mod_net)
-plot(mod_net)
+
+mod_net <- cv.glmnet(
+  x, log(y + 0.001), 
+  alpha = 1,         
+  nfolds = 10        
+)
 summary(mod_net)
+plot(mod_net)
+coef(mod_net, s = "lambda.min")
+
+ggplot(data = dat_all_seas, aes(x = log(Z), y = log(RW))) +
+  geom_point(alpha=0.1) + geom_smooth(method = "lm")
+
 coef(mod_net, s = "lambda.min")
 coefficients <- as.data.frame(as.matrix(coef(mod_net, s = "lambda.1se")))
 colnames(coefficients) <- "Coefficient"
